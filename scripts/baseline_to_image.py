2930211747988247d7892345b56d73bf174bcda4
Vincent Feistel <vafdaf11@gmail.com>
2025-09-17 09:40:11 +0200

    feat: Implement batch processing of datasets

--- scripts/baseline_to_image.py
+++ scripts/baseline_to_image.py
@@ -1,5 +1,9 @@
 import h5py
 import numpy as np
+from dataclasses import dataclass
+from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
 
 def inpaint_clean(file: h5py.File, baseline: str) -> np.ndarray:
     """In-paints an RFI-corrupted baseline using the data predicted by CLEAN"""
@@ -41,6 +45,11 @@ def inpaint_zero(file: h5py.File, baseline: str) -> np.ndarray:
     
     return eta
 
+@dataclass
+class Args:
+    dataset: str
+    targetdir: str
+    method: str
 
 METHODS = {
     "clean": inpaint_clean,
@@ -53,47 +62,73 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(
         prog="Baseline to Image Conversion",
-        description="In-paints a baseline from the input dataset, writing the result to a .npy file for processing",
+        description="In-paint and standardize baselines from an HDF5 dataset.",
     )
-    parser.add_argument("input", help=".h5 file containing the baseline data")
-    parser.add_argument("base", help="name of the baseline to extract")
-    parser.add_argument("--name",
+    parser.add_argument("dataset", help=".h5 file containing the baseline data")
+    parser.add_argument("--targetdir",
         required=True,
-        help="Prefix of output .npy files",
+        help="Directory to store the different .npy files",
     )
+
     parser.add_argument("--method",
         choices=list(METHODS.keys()),
         default="clean",
         help="method used for inpainting",
     )
 
-    args = parser.parse_args()
-    infile = args.input
-    baseline = args.base
-    name = args.name
-    method = args.method
+    args = Args(**vars(parser.parse_args()))
     polarization = 0 # TODO: Add CLI argument for this
+    
+    dir_root = Path(args.targetdir)
+    dir_root.mkdir(parents=True, exist_ok=True)
 
-    with h5py.File(infile, "r+") as file:
-        # Data coming from the telescope
-        dark = file[f"{baseline}/CORRUPTED_DATA"][:, :, polarization]
-        dark = np.abs(dark)
-        
-        # In-painted result
-        eta = METHODS[method](file, baseline)
+    dir_dark = dir_root.joinpath("dark")
+    dir_dark.mkdir(exist_ok=True)
 
-        # Standardize the result according to the unflagged visibilities
-        flags = file[f"{baseline}/FLAG"][:, :, polarization]
-        mean, std = dark[~flags].mean(), dark[~flags].std()
-        dark = (dark - mean) / std
-        eta = (eta - mean) / std
+    dir_eta = dir_root.joinpath("eta")
+    dir_eta.mkdir(exist_ok=True)
 
-        # Expand dimensions to be in form WxHx1
-        dark = np.expand_dims(dark, axis=2)
-        eta = np.expand_dims(eta, axis=2)
+    dir_target = dir_root.joinpath("target")
+    dir_target.mkdir(exist_ok=True)
 
-        # Finally, write to file
-        np.save(f"{name}-dark.npy", dark, allow_pickle=False)
-        np.save(f"{name}-eta.npy", eta, allow_pickle=False)
+
+    stats = []
+    with h5py.File(args.dataset, "r+") as file:
+        for key in tqdm(file.keys(), total=len(file)):
+            # Uncorrupted data from the telescope
+            target = file[f"{key}/CORRECTED_DATA"][:, :, polarization]
+            target = np.abs(target)
+
+            # Corrupted data from the telescope
+            dark = file[f"{key}/CORRUPTED_DATA"][:, :, polarization]
+            dark = np.abs(dark)
+            
+            # In-painted result
+            eta = METHODS[args.method](file, key)
+
+            # Standardize the result according to the unflagged visibilities
+            flags = file[f"{key}/FLAG"][:, :, polarization]
+            mean, std = dark[~flags].mean(), dark[~flags].std()
+            dark = (dark - mean) / std
+            eta = (eta - mean) / std
+            target = (target - mean) / std
+
+            # Expand dimensions to be in form WxHx1
+            dark = np.expand_dims(dark, axis=2)
+            eta = np.expand_dims(eta, axis=2)
+
+            # Finally, write to file
+            np.save(dir_eta.joinpath(f"{key}.npy"), eta, allow_pickle=False)
+            np.save(dir_dark.joinpath(f"{key}.npy"), dark, allow_pickle=False)
+            np.save(dir_target.joinpath(f"{key}.npy"), target, allow_pickle=False)
+            stats.append({
+                "baseline": key,
+                "mean": mean,
+                "stdev": std,
+            })
+
+    df = pd.DataFrame(stats)
+    df.set_index("baseline", inplace=True)
+    df.to_csv(dir_root.joinpath("summary.csv"))
 
 
